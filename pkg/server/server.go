76a87646840600aacc7c6f87b4360a62099b6a43
damasdev <damas@majoo.id>
2023-06-22 16:15:33 +0700

    init fiber

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,9 @@
 package server
 
-import "github.com/damasdev/fiber/pkg/logger"
+import (
+	"github.com/damasdev/fiber/pkg/logger"
+	"github.com/gofiber/fiber/v2"
+)
 
 type Server interface {
 	Run()
@@ -17,5 +20,13 @@ func New(logger logger.Logger) Server {
 }
 
 func (srv *server) Run() {
-	srv.logger.Info("server is running..")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	if err := app.Listen(":3000"); err != nil {
+		srv.logger.Fatal("failed to start server", err)
+	}
 }
