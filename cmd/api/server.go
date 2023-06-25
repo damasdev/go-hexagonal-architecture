f2dc5287a2ce64fd022bb4bea00356b8772030a7
damasdev <damas@majoo.id>
2023-06-25 09:44:01 +0700

    move init

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,13 @@
 package api
 
-import "github.com/damasdev/fiber/pkg/server"
+import (
+	"github.com/damasdev/fiber/pkg/config"
+	"github.com/damasdev/fiber/pkg/server"
+)
+
+func init() {
+	config.LoadEnvVars()
+}
 
 func Run() {
 	server.NewFiber().Run()
--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"github.com/damasdev/fiber/cmd"
-	"github.com/damasdev/fiber/pkg/config"
 )
-
-func init() {
-	config.LoadEnvVars()
-}
 
 func main() {
 	cmd.Run()
