205f1dbf232a96949bac86fd0b5e05a1488c3eaf
damasdev <damas@majoo.id>
2023-07-05 22:14:55 +0700

    change server entry point

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,27 +1,61 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
 
-	"github.com/damasdev/fiber/internal/interfaces/command/server"
-	"github.com/damasdev/fiber/pkg/cli"
 	"github.com/damasdev/fiber/pkg/config"
+	"github.com/damasdev/fiber/pkg/logger"
+	"github.com/damasdev/fiber/pkg/server"
+	"github.com/rs/zerolog/diode"
 )
 
 func init() {
 	config.LoadEnvVars()
+
+	level, err := strconv.ParseInt(os.Getenv("LOG_THRESHOLD"), 10, 64)
+	if err != nil {
+		level = int64(logger.WarnLevel)
+	}
+
+	logger.Initialize(
+		logger.WithWriter(
+			diode.NewWriter(os.Stdout, 1000, 10*time.Millisecond, func(missed int) {
+				fmt.Printf("Logger Dropped %d messages", missed)
+			}),
+		),
+		logger.WithLevel(logger.LogLevel(level)),
+		logger.WithName(os.Getenv("APP_NAME")),
+	)
 }
 
 func main() {
 
-	cli := cli.NewCLI()
+	server := server.New()
 
-	// register all command-line
-	cli.RegisterCommand(
-		server.Command(),
-	)
+	server.RegisterMiddleware()
+	server.RegisterHook()
+	server.RegisterHandler()
 
-	if err := cli.Run(); err != nil {
-		log.Fatal(err)
+	go func() {
+		if err := server.Run(); err != nil {
+			log.Fatal("Shutting down the server.")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Stop(ctx); err != nil {
+		log.Fatal(err.Error())
 	}
 }
