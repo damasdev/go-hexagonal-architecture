92cc79f30fc337b2283e659d2a0ec955a6a0eb52
damasdev <damas@majoo.id>
2023-07-06 11:49:18 +0700

    rename format json

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,6 +1,6 @@
 package response
 
-type format struct {
+type formatJson struct {
 	Status struct {
 		Code    int    `json:"code"`
 		Message string `json:"message"`
@@ -10,6 +10,6 @@ type format struct {
 	Errors interface{} `json:"errors,omitempty"`
 }
 
-func New() format {
-	return format{}
+func New() formatJson {
+	return formatJson{}
 }
